27bef7d4afc3043107a4b650a5d28f82368e26db
Artur Fedorov <arturmini@mail.ru>
2024-03-18 20:41:13 +0700

    Добавлена функция бесследного удаления файла. Тест пройден.

--- FileAndDirectoryDelete.py
+++ FileAndDirectoryDelete.py
@@ -2,8 +2,26 @@ import os
 import shutil
 
 class FileAndDirectoryDelete:
+    def __init__(self, overwrite_times=10, overwrite_char='0'):
+        self.overwrite_times = overwrite_times
+        self.overwrite_char = overwrite_char
 
     def is_exists(self, path):
         return os.path.exists(path)
+
+    def destroy_file(self, file_path):
+        if not self.is_exists(file_path):
+            print(f"Файл {file_path} не найден.")
+            return
+
+        with open(file_path, "r+") as file:
+            length = os.path.getsize(file_path)
+            for _ in range(self.overwrite_times):
+                file.seek(0)
+                file.write(self.overwrite_char * length)
+        os.remove(file_path)
+        print(f"Файл {file_path} успешно уничтожен.")
+
+
 
 
