70f71fe757e3a1980cf3a24790421e72b84a1b91
Artur Fedorov <arturmini@mail.ru>
2024-03-18 20:18:54 +0700

    Добавлены тесты на проверку существования файла и папки

--- Test/Test_FileAndDirectoryDelete.py
+++ Test/Test_FileAndDirectoryDelete.py
@@ -12,30 +12,30 @@ class TestFileAndDirectoryDelete(unittest.TestCase):
 
         self.assertIsNotNone(file_dir_delete)
 
-    def setUp(self):
-        self.temp_file = tempfile.NamedTemporaryFile(delete=False)
-        self.temp_file_path = self.temp_file.name
-        self.temp_dir = tempfile.TemporaryDirectory()
-        self.temp_dir_path = self.temp_dir.name
-
-    def tearDown(self):
-        os.unlink(self.temp_file_path)
-
     def test_is_exists_file_True(self):
+        temp_file = tempfile.NamedTemporaryFile(delete=False)
+        temp_file_path = temp_file.name
         file_dir_delete = FileAndDirectoryDelete()
-        self.assertTrue(file_dir_delete.is_exists(self.temp_file_path))
+        self.assertTrue(file_dir_delete.is_exists(temp_file_path))
+        temp_file.close()
+        os.unlink(temp_file_path)
 
     def test_is_exists_file_False(self):
         file_dir_delete = FileAndDirectoryDelete()
-        self.assertFalse(file_dir_delete.is_exists(self.temp_file_path))
+        self.assertFalse(file_dir_delete.is_exists("/non/existent/path"))
 
     def test_is_exists_dir_True(self):
+        temp_dir = tempfile.TemporaryDirectory()
+        temp_dir_path = temp_dir.name
         file_dir_delete = FileAndDirectoryDelete()
-        self.assertTrue(file_dir_delete.is_exists(self.temp_dir_path))
+        self.assertTrue(file_dir_delete.is_exists(temp_dir_path))
+        temp_dir.cleanup()
+
+
 
     def test_is_exists_dir_False(self):
         file_dir_delete = FileAndDirectoryDelete()
-        self.assertFalse(file_dir_delete.is_exists(self.temp_dir_path))
+        self.assertFalse(file_dir_delete.is_exists("/non/existent/path"))
 
 
 
